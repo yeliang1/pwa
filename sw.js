66df1bb93677857beda635b1ed3e08479929b6d3
yeliang <yeliang@jd.com>
2019-02-27 15:18:26 +0800

    sw3

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 var cacheName = 'bs-0-2-0';
 var cacheFiles = [
-    '/',
     'index.html',
     'index.js',
     'style.css',
@@ -15,4 +14,16 @@ self.addEventListener('install', function (e) {
         return cache.addAll(cacheFiles);
     });
     e.waitUntil(cacheOpenPromise);
+});
+
+self.addEventListener('fetch', function (e) {
+    // 如果有cache则直接返回，否则通过fetch请求
+    e.respondWith(
+        caches.match(e.request).then(function (cache) {
+            return cache || fetch(e.request);
+        }).catch(function (err) {
+            console.log(err);
+            return fetch(e.request);
+        })
+    );
 });
