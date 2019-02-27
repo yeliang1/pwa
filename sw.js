f1e020e70a34739f4edf35f2d903603e7c37ed37
yeliang <yeliang@jd.com>
2019-02-27 15:02:39 +0800

    sw2

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-// 监听install事件
+var cacheName = 'bs-0-2-0';
+var cacheFiles = [
+    '/',
+    'index.html',
+    'index.js',
+    'style.css',
+    'img/book.png',
+    'img/loading.svg'
+];
+
+// 监听install事件，安装完成后，进行文件缓存
 self.addEventListener('install', function (e) {
     console.log('Service Worker 状态： install');
+    var cacheOpenPromise = caches.open(cacheName).then(function (cache) {
+        return cache.addAll(cacheFiles);
+    });
+    e.waitUntil(cacheOpenPromise);
 });
